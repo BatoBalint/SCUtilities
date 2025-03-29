9597500dfe426e340609c58bc64cca81ed0525e8
Vargha Simon <wfrh7n@inf.elte.hu>
2025-03-29 19:10:56 +0100

    Base stuff
    - DaisyUI init
    - Prettier Tailwind plugin
    - Routing base
        - Landing page
        - 404 page
        - Redirect anything else to 404
    - Some primitive css (dark mode)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponentComponent } from './pages/page-not-found-component/page-not-found-component.component';
+import { LandingPageComponentComponent } from './pages/landing-page-component/landing-page-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'SCUtilities',
+    component: LandingPageComponentComponent
+  },
+  {
+    path: '404',
+    title: 'Page not found',
+    component: PageNotFoundComponentComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/pages/landing-page-component/landing-page-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-component',
+  imports: [],
+  templateUrl: './landing-page-component.component.html',
+  styleUrl: './landing-page-component.component.css'
+})
+export class LandingPageComponentComponent {
+
+}
