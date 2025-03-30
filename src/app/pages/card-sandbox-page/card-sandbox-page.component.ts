0f5db845118a42edd110076236df76c6804e5aca
Bato Balint <balint.bato@gmail.com>
2025-03-30 19:06:49 +0200

    Example card added

--- src/app/pages/card-sandbox-page/card-sandbox-page.component.ts
+++ src/app/pages/card-sandbox-page/card-sandbox-page.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { CustomCardComponent } from '../../custom-card/custom-card.component';
 
 @Component({
   selector: 'app-card-sandbox-page',
-  imports: [],
+  imports: [CustomCardComponent, NgFor],
   templateUrl: './card-sandbox-page.component.html',
   styleUrl: './card-sandbox-page.component.css'
 })
 export class CardSandboxPageComponent {
+  nums: Number[] = []
 
+  constructor() {
+    this.myFunc();
+  }
+
+  myFunc() {
+    for (let i = 0; i < 10; i++) {
+      this.nums.push(i);
+    }
+  }
 }
